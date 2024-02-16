a6c38c603dbb2178cf3bc2748641f0cb63badd5e
julesdelau <101152029+julesdelau@users.noreply.github.com>
2024-02-16 10:17:08 +0100

    test2

---
+++ clone/src/main/java/com/mycompany/clone/Clone.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.clone;
+
+/**
+ *
+ * @author Jules_D
+ */
+public class Clone {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
